488c5108fbf687383a15efe94d02e528cffdb699
shriyakaram <69758690+shriyakaram@users.noreply.github.com>
2026-05-08 17:37:27 -0400

    head disc for pythia

--- Experiments/Pythia/head_disc_pythia.py
+++ Experiments/Pythia/head_disc_pythia.py
@@ -25,7 +25,6 @@ from eval_utils import (
 )
 
 RESULTS_DIR = Path(__file__).parent / "Results"
-RESULTS_DIR.mkdir(parents=True, exist_ok=True)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
